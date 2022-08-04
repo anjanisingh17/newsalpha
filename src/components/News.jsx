196ca6183192d9407f0062e1c3c0b7e751f6911c
Anjani <anjanisingh953@gmail.com>
2022-08-04 19:03:30 +0530

    last commit

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,68 +2,64 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-
-    articles = [
-        {
-        "source": {
-        "id": "bbc-sport",
-        "name": "BBC Sport"
-        },
-        "author": "BBC Sport",
-        "title": "Shane Warne memorial - watch & follow updates",
-        "description": "Watch live coverage and follow text updates and tributes from the state memorial for Australian cricket legend Shane Warne at the Melbourne Cricket Ground.",
-        "url": "http://www.bbc.co.uk/sport/live/cricket/60916236",
-        "urlToImage": "https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.22.0/images/bbc-sport-logo.png",
-        "publishedAt": "2022-03-30T08:22:26.498888Z",
-        "content": "Former England bowler and BBC cricket presenter Isa Guha, who became a colleague of Warne's in the commentary box: \"It has been a strange few weeks - a lot of shock and then we did our own tribute at… [+396 chars]"
-        },
-        {
-        "source": {
-        "id": "espn-cric-info",
-        "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-        "publishedAt": "2020-04-27T11:41:47Z",
-        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-        "source": {
-        "id": "espn-cric-info",
-        "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-        "publishedAt": "2020-03-30T15:26:05Z",
-        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-        ]
     
 
         constructor(){
             super();
             console.log('Inside constructor body');
             this.state = {
-                    articles : this.articles,
-                    loading : false
+                    articles : [],
+                    loading : false,
+                    page:1
             }
         }    
 
         async componentDidMount(){
-            let url = 'https://newsapi.org/v2/top-headlines?country=in&category=sport&apiKey=3ba5e7b3ba4e44c7b922ea03462be5d6';
+            let url = 'https://newsapi.org/v2/top-headlines?country=in&category=sport&apiKey=3ba5e7b3ba4e44c7b922ea03462be5d6&page=1&pageSize=20';
+            let data = await fetch(url);
+            let result = await data.json();
+            console.log(result);   
+            let total_result = result.totalResults;
+            console.log('total',total_result);
+
+            this.setState({
+                articles: result.articles
+            })
+        }
+
+        //handlePrevClick funciton
+        handlePrevClick = async()=>{
+
+            let url = `https://newsapi.org/v2/top-headlines?country=in&category=sport&apiKey=3ba5e7b3ba4e44c7b922ea03462be5d6&page=${this.state.page-1}&pageSize=20`;
             let data = await fetch(url);
             let result = await data.json();
             console.log(result);
             this.setState({
                 articles: result.articles
             })
+
+            console.log('prev')
+            this.setState({
+                page: this.state.page-1
+            })
         }
+        
+        handleNextClick = async()=>{
+
+            let url = `https://newsapi.org/v2/top-headlines?country=in&category=sport&apiKey=3ba5e7b3ba4e44c7b922ea03462be5d6&page=${this.state.page-1}&pageSize=20`;
+            let data = await fetch(url);
+            let result = await data.json();
+            console.log(result);
+            this.setState({
+                articles: result.articles
+            })
+
+            console.log('next')
+            this.setState({
+                page: this.state.page+1
+            })
+        }
+
 
   render() {
     return (
@@ -75,14 +71,17 @@ export class News extends Component {
 
       return(
         
-        <NewsItem key={index} title={curElement.title?curElement.title.slice(0,50):""} description={curElement.description?curElement.description.slice(0,80):"This is description for the above title"} imgurl={curElement.urlToImage} newsurl={curElement.url} />
+        <NewsItem key={index} title={curElement.title?curElement.title.slice(0,50):"This is title"} description={curElement.description?curElement.description.slice(0,80):"This is description for the above title"} imgurl={curElement.urlToImage?curElement.urlToImage:'https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg'} newsurl={curElement.url} publishedAt={curElement.publishedAt.slice(0,10)} author={curElement.author} />
 
       )
     
   })}
 
-
      </div> 
+        <div className='container d-flex justify-content-between my-4'>
+        <button disabled={this.state.page <=1 } type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+        <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>  Next &rarr;</button>
+        </div>
     </div>
 
       </>  
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -4,7 +4,7 @@ export class NewsItem extends Component {
   
   render() {
 
-    let {title,description,imgurl,newsurl} = this.props;
+    let {title,description,imgurl,newsurl,publishedAt,author} = this.props;
 
     return (
         <div className="col-lg-4 col-md-4 col-sm-1 card-group my-3" >
@@ -13,10 +13,10 @@ export class NewsItem extends Component {
           <div className="card-body">
               <h5 className="card-title">{title} ...</h5>
               <p className="card-text">{description} ...</p>
-              <a href={newsurl} className='btn btn-sm btn-primary' target='_blank'>Read More</a>
+              <a href={newsurl} rel='noreferrer' className='btn btn-sm btn-primary' target='_blank'>Read More</a>
           </div>
           <div className="card-footer">
-            <small className="text-muted">Last updated 3 mins ago</small>
+            <small className="text-muted"><div className='date_div' style={{float:'left',color:'#fff',background:'#000',padding:'5px 20px'}}>  {publishedAt} </div> <div className='author_div' style={{float:'right',color:'#fff',background:'#7c0707b8',padding:'5px 15px'}}>Author: {author}</div> </small>
           </div>
         </div>
       </div>   
